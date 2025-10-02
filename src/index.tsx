ca3e153bc3a66be2d3030c8e4bad933e2fc7035f
kurabegomu-spec <kurabegomu@gmail.com>
2025-10-02 13:40:13 +0700

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './tailwind.css';
-import App from './App';
+import App from './app';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <app />
   </React.StrictMode>
 );
